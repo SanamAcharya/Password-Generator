2f03d97010f624bcf58614c0a2e3d6b6be42bb05
sanamacharya <heyesanam@gmail.com>
2024-07-28 18:41:24 +0545

    Added option to select length of password

--- script.js
+++ script.js
@@ -1,94 +1,54 @@
 let textFiledEl = document.getElementById("text-field-el");
 let copyBtnEl = document.getElementById("copy-btn-el");
 let generateBtnEl = document.getElementById("generate-btn-el");
+let sliderEl = document.getElementById("slider-el");
+let sliderValueEl = document.getElementById("slider-value");
+let lowercaseCheckbox = document.getElementById("lowercase-checkbox");
+let uppercaseCheckbox = document.getElementById("uppercase-checkbox");
+let numbersCheckbox = document.getElementById("numbers-checkbox");
+let symbolsCheckbox = document.getElementById("symbols-checkbox");
+
+sliderEl.addEventListener("input", () => {
+  sliderValueEl.textContent = sliderEl.value;
+});
 
 generateBtnEl.addEventListener("click", generate);
 
 function generate() {
-  let arr = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "-",
-    "_",
-    "=",
-    "+",
-  ];
+  let lowercase = "abcdefghijklmnopqrstuvwxyz";
+  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numbers = "0123456789";
+  let symbols = "!@#$%^&*()-_=+";
+
+  let availableCharacters = "";
+
+  if (lowercaseCheckbox.checked) {
+    availableCharacters += lowercase;
+  }
+  if (uppercaseCheckbox.checked) {
+    availableCharacters += uppercase;
+  }
+  if (numbersCheckbox.checked) {
+    availableCharacters += numbers;
+  }
+  if (symbolsCheckbox.checked) {
+    availableCharacters += symbols;
+  }
+
+  if (availableCharacters === "") {
+    alert("Please select at least one character type.");
+    return;
+  }
+
   copyBtnEl.innerHTML = "Copy";
   let password = "";
-  let passwordLength = 14;
+  let passwordLength = sliderEl.value;
 
   for (let index = 0; index < passwordLength; index++) {
-    let char = arr[Math.floor(Math.random() * arr.length)];
+    let char =
+      availableCharacters[
+        Math.floor(Math.random() * availableCharacters.length)
+      ];
     password += char;
   }
   textFiledEl.value = password;
